f62a88257e514cea34170af07d206d100cdc47d5
luxshanasiva <lakshanasiva98@gmail.com>
2022-12-02 20:49:12 +0530

    first commit

---
+++ Componets/Main/index.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import Home from '../Home';
+import Detail from '../Detail';
+import Setting from '../Setting';
+
+//Screen names
+const homeName = "Home";
+const detailsName = "Detail";
+const settingsName = "Setting";
+
+const Tab = createBottomTabNavigator();
+
+function Main() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === detailsName) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } else if (rn === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={Home} />
+        <Tab.Screen name={detailsName} component={Detail} />
+        <Tab.Screen name={settingsName} component={Setting} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Main;
